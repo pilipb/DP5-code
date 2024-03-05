b06ddf2c03bf6ae9fb71ee737d88392e93d5d567
pilipb <xq19351@bristol.ac.uk>
2024-03-05 13:31:59 +0000

    long term power model done

--- arima_functions.py
+++ arima_functions.py
@@ -5,6 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 
+import requests
+import pandas as pd
+
+# script for returning elevation from lat, long, based on open elevation data
+# which in turn is based on SRTM
+def get_elevation(lat, long):
+    query = ('https://api.open-elevation.com/api/v1/lookup'
+             f'?locations={lat},{long}')
+    r = requests.get(query).json()  # json object, various ways you can extract value
+    # one approach is to use pandas json functionality:
+    elevation = pd.json_normalize(r, 'results')['elevation'].values[0]
+    return elevation
+
 # Function to read the data from the file
 def read_data(file_path):
     metadata = {}
