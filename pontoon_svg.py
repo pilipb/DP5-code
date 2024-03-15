c476363f5efa905e6423bed1a7916b34d7170cc5
pilipb <xq19351@bristol.ac.uk>
2024-03-15 17:52:54 +0000

    dimensions update and different file types

--- pontoon_svg.py
+++ pontoon_svg.py
@@ -6,18 +6,33 @@ from aerofoil import naca_foil
 x,y = naca_foil(0.25,0)
 
 # move the foil to the right
-x = [i+0.5 for i in x]
-# move the foil down
-y = [i+0.5 for i in y]
+# x = [i+0.5 for i in x]
+# # move the foil down
+# y = [i+0.5 for i in y]
 
 # scale to
-size = 30 # cm
+size = 1800 # cm
 x = [i*size for i in x]
 y = [i*size for i in y]
+z = [0 for i in x]
 
 
 # make an svg file with the foil coordinates
 dwg = svgwrite.Drawing('pontoon.svg', profile='tiny')
 dwg.add(dwg.polyline(points=[(x[i],y[i]) for i in range(len(x))], fill='none', stroke='black'))
 dwg.save()
+
+# save the foil coordinates to a csv file
+f = open('pontoon.csv','w')
+for i in range(len(x)):
+    f.write(str(x[i])+','+str(y[i])+','+str(z[i])+'\n')
+f.close()
+
+# save the foil coordinates to a txt file
+f = open('pontoon.txt','w')
+for i in range(len(x)):
+    f.write(str(x[i])+' '+str(y[i])+' '+str(z[i])+'\n')
+f.close()
+
+
 
